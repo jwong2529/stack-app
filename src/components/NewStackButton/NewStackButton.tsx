dfd2c1d14fcfc37670036ceb16410e4392585fcc
jwong2529 <janice.wong2529@gmail.com>
2024-05-22 02:21:58 -0700

    Create new stack button, rearrange files

---
+++ src/components/NewStackButton/NewStackButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewStackButton.css';
+
+const NewStackButton: React.FC = () => {
+    return (
+        <button className="new-stack-button">
+            Create new stack
+        </button>
+    )
+}
+
+export default NewStackButton;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Banner from '../components/Banner';
+import Banner from '../../components/Banner/Banner';
+import NewStackButton from '../../components/NewStackButton/NewStackButton';
 
 const HomePage: React.FC = () => {
     return (
         <div className="home-container">
             <Banner/>
+            <NewStackButton/>
         </div>
     );
 };
