fd28259ee26c1be299d57c2fa2ea4c7f7becae27
jwong2529 <janice.wong2529@gmail.com>
2024-05-21 21:25:16 -0700

    Rearrange files

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner:React.FC = () => {
+    return (
+        <div className="banner">
+            <div className="banner-app-title">
+                Stack
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../components/Banner';
+
+const HomePage: React.FC = () => {
+    return (
+        <div className="home-container">
+            <Banner/>
+        </div>
+    );
+};
+
+export default HomePage;
