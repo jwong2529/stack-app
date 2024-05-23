264ccb0369669c4567b7bdab4ca4d61ec49660a0
jwong2529 <janice.wong2529@gmail.com>
2024-05-22 22:51:54 -0700

    Create stack card list w/ flex wrap layout

---
+++ src/components/StackCard/StackCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StackCard.css';
+
+const StackCard: React.FC = () => {
+    return (
+        <div className="stack-card">
+            <h3 className="stack-card-title">Leetcode</h3>
+        </div>
+    )
+}
+
+export default StackCard;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import './HomePage.css';
 import Banner from '../../components/Banner/Banner';
 import NewStackButton from '../../components/NewStackButton/NewStackButton';
+import StackCard from '../../components/StackCard/StackCard';
 
 const HomePage: React.FC = () => {
     return (
         <div className="home-container">
             <Banner/>
             <NewStackButton/>
+            <div className="stacks-container">
+                My Stacks
+                <div className="stack-card-list">
+                    <StackCard/>
+                </div>
+            </div>
         </div>
     );
 };
