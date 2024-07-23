723c72dff4c0befafcaafa5cc566111b208fbdf9
jwong2529 <janice.wong2529@gmail.com>
2024-07-23 00:21:16 -0700

    create login + signup page layouts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginSignup/LoginPage';
+import SignupPage from './pages/LoginSignup/SignupPage';
 import StackPage from './pages/StackPage/StackPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/stack/:uuid" element={<StackPage />} />
       </Routes>
     </Router>
